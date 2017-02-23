d1af7633b43670e4f925d0589f1ff3084af41bf0
Laurence <laurence.j.squires@gmail.com>
2017-02-23 17:55:12 +0000

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,16 @@ import java.util.List;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    String location = "";
-    List<String> data = Files.readAllLines(Paths.get(location));
+      String location = "";
+      List<String> data = Files.readAllLines(Paths.get(location));
+      String[] firstLine = data.get(0).split(" ");
+      int noOfVideos = Integer.parseInt(firstLine[0]);
+      int noOfEndPoints = Integer.parseInt(firstLine[1]);
+      int noOfRequestDesc = Integer.parseInt(firstLine[2]);
+      int noOfCache = Integer.parseInt(firstLine[3]);
+      int cacheSize = Integer.parseInt(firstLine[4]);
+
+
   }
+
 }

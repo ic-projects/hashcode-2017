0eaa03318148741e54899911b5accd94369a54cd
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2017-02-23 18:30:16 +0000

    Added cache id

--- src/CacheServer.java
+++ src/CacheServer.java
@@ -2,8 +2,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CacheServer {
+  public int id;
   public HashMap<Integer, Integer> latencyToEndpoint = new HashMap<>();
   public ArrayList<Video> videos = new ArrayList<>();
+  public HashMap<Video, Integer> videoValues = new HashMap<>();
   public int maxCapacity;
 
   public CacheServer(int cacheSize) {

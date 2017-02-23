bc6a0cd3bc1c43e506dd18f4f009b746e3e360f7
Laurence <laurence.j.squires@gmail.com>
2017-02-23 18:42:52 +0000

    finished main maybe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.awt.datatransfer.StringSelection;
+import java.io.InputStream;
 import java.lang.reflect.Array;
+import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
@@ -10,8 +14,12 @@ public class Main {
     public static ArrayList<CacheServer> cacheServers  = new ArrayList<>();
 
   public static void main(String[] args) throws Exception {
-      String location = "";
-      List<String> data = Files.readAllLines(Paths.get(location));
+      String location = "/data/sample.in";
+      String pre = Paths.get(".").toAbsolutePath().normalize().toString();
+      List<String> data = Files.readAllLines(Paths.get(pre+location));
+      for(int i = 0; i < data.size(); i++) {
+          //System.out.println(data.get(i));
+      }
       String[] firstLine = data.get(0).split(" ");
       int noOfVideos = Integer.parseInt(firstLine[0]);
       int noOfEndPoints = Integer.parseInt(firstLine[1]);
@@ -19,6 +27,8 @@ public class Main {
       int noOfCache = Integer.parseInt(firstLine[3]);
       int cacheSize = Integer.parseInt(firstLine[4]);
 
+      System.out.println(noOfVideos+","+noOfEndPoints+","+noOfRequestDesc
+      +","+noOfCache+","+cacheSize);
       for(int i = 0; i < noOfCache ;i++) {
           cacheServers.add(new CacheServer(cacheSize));
       }
@@ -33,7 +43,9 @@ public class Main {
       int offset = 0;
       int lastOffset = 0;
       for(int i = 0 ; i < noOfEndPoints; i++) {
+
           String[] ep = data.get(2+ i + offset).split(" ");
+          System.out.println("endpoint: "+ String.join(",",ep));
           int datacenterLatency =  Integer.parseInt(ep[0]);
           int caches =  Integer.parseInt(ep[1]);
           Map<Integer, Integer> latencies = new HashMap<>();
@@ -53,7 +65,7 @@ public class Main {
           int vid =  Integer.parseInt(ep[0]);
           int end =  Integer.parseInt(ep[1]);
           int latency =  Integer.parseInt(ep[2]);
-          endpoints.get(end).addRequest(vid,latency);
+          //endpoints.get(end).addRequest(vid,latency);
       }
 
 

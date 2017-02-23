6fb963bb5dbd40e59b03d993b4354f7086314613
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2017-02-23 17:20:15 +0000

    Initialised Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    String location = "";
+    List<String> data = Files.readAllLines(Paths.get(location));
+  }
+}

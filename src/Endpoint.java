2179950807cbb65238f480993646b7a85449a047
Alexander Clarke <ac3716@point45.doc.ic.ac.uk>
2017-02-23 18:33:06 +0000

    changed request adder

--- src/Endpoint.java
+++ src/Endpoint.java
@@ -1,14 +1,18 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class Endpoint {
   Map<Integer, Integer> servers;
   Integer latency;
+  HashMap<Video, Integer> requests
   Endpoint (Map<Integer, Integer> serverslatency, Integer latency) {
     servers = serverslatency;
     this.latency = latency;
-    this.requests = requests;
   }
 
+  public void addRequest(Video video, Integer latency) {
+    requests.put(video, latency);
+  }
 
 
 }

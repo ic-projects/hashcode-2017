c8d8396383e06b8bde2d42d299317f3295d16d51
Laurence <laurence.j.squires@gmail.com>
2017-02-23 18:23:52 +0000

    finished main maybe

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
 import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.IntSummaryStatistics;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static ArrayList<Video> videos = new ArrayList<>();
-    public static ArrayList<Video> endpoints  = new ArrayList<>();
+    public static ArrayList<Endpoint> endpoints  = new ArrayList<>();
     public static ArrayList<CacheServer> cacheServers  = new ArrayList<>();
 
   public static void main(String[] args) throws Exception {
@@ -34,21 +31,30 @@ public class Main {
       }
 
       int offset = 0;
+      int lastOffset = 0;
       for(int i = 0 ; i < noOfEndPoints; i++) {
           String[] ep = data.get(2+ i + offset).split(" ");
           int datacenterLatency =  Integer.parseInt(ep[0]);
           int caches =  Integer.parseInt(ep[1]);
-          HashMap<Integer, Integer> latencies = new HashMap<>();
+          Map<Integer, Integer> latencies = new HashMap<>();
 
           for(int i2 = 0 ;i2 < caches; i2++) {
               String[] laten = data.get(2 + i + offset + i2 + 1).split(" ");
               latencies.put(Integer.parseInt(laten[0]), Integer.parseInt(laten[1]));
+              lastOffset = 2 + i + offset + i2 + 1;
           }
           offset = caches;
-          Endpoint endpoint = new Endpoint(latencies);
+          Endpoint endpoint = new Endpoint(latencies, datacenterLatency);
           endpoints.add(endpoint);
       }
 
+      for(int i = 0 ; i < noOfRequestDesc; i++) {
+          String[] ep = data.get(1 + i + lastOffset).split(" ");
+          int vid =  Integer.parseInt(ep[0]);
+          int end =  Integer.parseInt(ep[1]);
+          int latency =  Integer.parseInt(ep[2]);
+          endpoints.get(end).addRequest(vid,latency);
+      }
 
 
   }
